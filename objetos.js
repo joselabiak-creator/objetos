5b0bad82c14335a8af1e5b567c03c75038b4b9ee
joselabiak-creator <jose.labiak@escola.pr.gov.br>
2026-08-18 15:19:49 +0000

    ;;;

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const listaCPFs = [1111111, 22222, 3333];
+
+const informacoesPessoa  = ["nome", "jose", "idade","CPF", "111122223333"];
+
+console.log(informacoesPessoa[1]);
+
+const objetoPesso = {
+    idade: 32,
+    nome: "jose"
+};
