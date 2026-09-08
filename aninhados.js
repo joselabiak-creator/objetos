3d3c38e6179b328e2030294c765f2304f32eaf5a
joselabiak-creator <jose.labiak@escola.pr.gov.br>
2026-09-08 15:23:48 +0000

    r6srdctfvgybhnuje4c5rv6t8byumi,k ddfghjnml

---
+++ aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: [1234567812346,0987654321976],
+};
+
+cliente.endereco = {
+  rua: "Rua Joseph Climber",
+  numero: 1337,
+  apartamento: true,
+  complemento: "ap 934",
+};
+
+console.log(cliente);
